3d2f5c850765aa8d3bd05049377401d544292862
Charles Li <charles9li@berkeley.edu>
2019-05-22 01:25:25 -0700

    working on retrieving name of pokemon

--- database/webscrape.py
+++ database/webscrape.py
@@ -2,3 +2,8 @@ import requests
 import urllib.request
 import time
 from bs4 import BeautifulSoup
+
+url = 'https://serebii.net/pokedex-sm/001.shtml'
+response = requests.get(url)
+soup = BeautifulSoup(response.text, "html.parser")
+print(soup.body.find_all('table')[1].find_all('tr'))
