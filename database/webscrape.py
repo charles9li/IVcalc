37bc242d346421af1cf725a77a87ddc169d61751
Charles Li <charles9li@berkeley.edu>
2019-05-22 12:11:10 -0700

    successfully retrieved pokemon name

--- database/webscrape.py
+++ database/webscrape.py
@@ -3,7 +3,24 @@ import urllib.request
 import time
 from bs4 import BeautifulSoup
 
-url = 'https://serebii.net/pokedex-sm/001.shtml'
+
+def print_child_names(item):
+    for child in item.children:
+        print(child.name)
+
+
+url = "https://serebii.net/pokedex-sm/001.shtml"
 response = requests.get(url)
-soup = BeautifulSoup(response.text, "html.parser")
-print(soup.body.find_all('table')[1].find_all('tr'))
+soup = BeautifulSoup(response.text, 'html5lib')
+table = soup.body.find_all('table')[1]
+body = table.find('tbody')
+row = body.contents[2]
+data = row.contents[3]
+font = data.font
+div = font.contents[2]
+div = div.div
+p = div.p
+table = p.table
+tbody = table.tbody
+row = tbody.contents[2]
+name = row.contents[3].text
