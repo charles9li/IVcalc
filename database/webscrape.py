7b4c909d18aa4de701d1a9a767a3160bc48c0414
Charles Li <charles9li@berkeley.edu>
2019-05-23 22:25:51 -0700

    put dex number, name, and base stat info into database

--- database/webscrape.py
+++ database/webscrape.py
@@ -1,15 +1,26 @@
-import requests
-import urllib.request
-import time
-from bs4 import BeautifulSoup
 from database.webscrape_utils import PokemonInfo
+import sqlite3
 
 
-def print_child_names(item):
-    for child in item.children:
-        print(child.name)
+# Create connection
+conn = sqlite3.connect('pokemon.db')
+c = conn.cursor()
 
+# Create table
+c.execute("CREATE TABLE IF NOT EXISTS data "
+          "(number, name, hp, atk, def, spa, spd, spe)")
 
-pokemon = PokemonInfo(1)
-# print_child_names(pokemon._stats_tag().parent.parent.parent.contents[3])
-print(pokemon._stats_tag().parent.parent.parent.contents[3].contents[2])
+# Insert pokemon data into table
+for dex_num in range(1, 808):
+    pokemon = PokemonInfo(dex_num)
+    c.execute("INSERT INTO data VALUES "
+              "(?, ?, ?, ?, ?, ?, ?, ?)",
+              (pokemon.dex_num, pokemon.name,
+               pokemon.base_stats[0], pokemon.base_stats[1],
+               pokemon.base_stats[2], pokemon.base_stats[3],
+               pokemon.base_stats[4], pokemon.base_stats[5]))
+    print(pokemon._dex_num_str + " " + pokemon.name + " "*(15 - len(pokemon.name)) + str(pokemon.base_stats))
+
+# Save (commit) the changes and close connection
+conn.commit()
+conn.close()
